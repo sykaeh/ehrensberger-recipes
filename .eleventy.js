ae37c3df3fd53bdd932d1435db9d0c9118dcc78e
Sybil Ehrensberger <sybil@ehrensberger.org>
2023-02-26 15:56:25 +0100

    Add responsive image

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,72 @@
 const htmlmin = require('html-minifier');
-const lazyImagesPlugin = require('eleventy-plugin-lazyimages');
+const Image = require('@11ty/eleventy-img');
+const outdent = require('outdent');
+
+/** Maps a config of attribute-value pairs to an HTML string
+ * representing those same attribute-value pairs.
+ */
+const stringifyAttributes = (attributeMap) => {
+  return Object.entries(attributeMap)
+    .map(([attribute, value]) => {
+      if (typeof value === 'undefined') return '';
+      return `${attribute}="${value}"`;
+    })
+    .join(' ');
+};
+
+const imageShortcode = async (
+  src,
+  alt,
+  className = undefined,
+  widths = [400, 800, 1280],
+  formats = ['webp', 'avif', 'jpeg'],
+  sizes = '100vw'
+) => {
+  const imageMetadata = await Image(src, {
+    widths: [...widths, null],
+    formats: [...formats, null],
+    outputDir: '_site/assets/images',
+    urlPath: '/assets/images',
+  });
+
+  const sourceHtmlString = Object.values(imageMetadata)
+    .map((images) => {
+      const { sourceType } = images[0];
+      const sourceAttributes = stringifyAttributes({
+        type: sourceType,
+        srcset: images.map((image) => image.srcset).join(', '),
+        sizes,
+      });
+      return `<source ${sourceAttributes}>`;
+    })
+    .join('\n');
+
+  const getLargestImage = (format) => {
+    const images = imageMetadata[format];
+    return images[images.length - 1];
+  };
+
+  const largestUnoptimizedImg = getLargestImage(formats[0]);
+  const imgAttributes = stringifyAttributes({
+    src: largestUnoptimizedImg.url,
+    width: largestUnoptimizedImg.width,
+    height: largestUnoptimizedImg.height,
+    alt,
+    loading: 'lazy',
+    decoding: 'async',
+  });
+  const imgHtmlString = `<img ${imgAttributes}>`;
+
+  const pictureAttributes = stringifyAttributes({
+    class: className,
+  });
+  const picture = `<picture ${pictureAttributes}>
+    ${sourceHtmlString}
+    ${imgHtmlString}
+  </picture>`;
+
+  return outdent`${picture}`;
+};
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter('makeList', function (value) {
@@ -10,16 +77,7 @@ module.exports = function (eleventyConfig) {
     }
   });
 
-  eleventyConfig.addPlugin(lazyImagesPlugin, {
-    transformImgPath: (imgPath) => {
-      if (imgPath.startsWith('http://') || imgPath.startsWith('https://')) {
-        // Handle remote file
-        return imgPath;
-      } else {
-        return `./src/${imgPath}`;
-      }
-    },
-  });
+  eleventyConfig.addShortcode('responsiveImage', imageShortcode);
 
   eleventyConfig.addTransform('htmlmin', (content, outputPath) => {
     if (outputPath.endsWith('.html')) {
